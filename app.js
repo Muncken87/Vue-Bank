2ba6b2b95052f23e8695635c5ae02e4c3ec341e5
Alexander <yumako87@gmail.com>
2017-07-22 15:21:42 +0200

    Fix filters for amount and date

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var app = new Vue({
       merchantLogo: '',
       transactionType: '',
       categoryCode: '',
-      search: ''
+      search: '',
+      isActive: false
   },
   methods: {
     // Push new withdrawal to results, takes the withdrawn money - the bankaccounts money
@@ -30,9 +31,32 @@ var app = new Vue({
        this.amount = '';
        this.merchant = '';
    },
-   filterAmount: function(){
-     console.log("Filtered!");
-   }
+   filterAmount: function() {
+     this.isActive = !this.isActive;
+     var self = this;
+
+      this.transfers.data.sort(function (a, b) {
+          if (self.isActive == true){
+            return a.amount - b.amount;
+          }
+          else {
+             return  b.amount -  a.amount;
+          }
+      });
+    },
+      filterDate: function() {
+        this.isActive = !this.isActive;
+        var self = this;
+
+         this.transfers.data.sort(function (a, b) {
+             if (self.isActive == true){
+               return a.transactionDate - b.transactionDate;
+             }
+             else {
+                return  b.transactionDate -  a.transactionDate;
+             }
+      });
+    }
  },
   // Fetches json array, and sets it as the transfers array that then gets looped in the html´s result field
   created() {
